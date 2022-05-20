5177b98139f13d4ca869270120d31a813b488adb
jinsu <qkwltn34@gmail.com>
2022-05-20 16:16:08 +0900

    hi3

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tv_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
